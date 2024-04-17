e9aa1ca4720467cab22bc1ab5b00b3e3617bf5d9
Luca Martino <alalalalal>
2024-04-17 20:55:02 -0300

    sumador v.1.02

--- sumador.c
+++ sumador.c
@@ -4,14 +4,16 @@
 
 int main (void) 
 {
-    int num1, num2, suma;
+    int num1, num2, num3, suma;
 
     p("ingrese un valor: ");
     s("%d",&num1);
     p("ingrese otro valor: ");
     s("%d", &num2);
-    suma = num1 + num2; 
-    p("%d + %d = %d", num1, num2, suma);
+    p("ingrese un tercer valor");
+    s("%d", &num3);
+    suma = num1 + num2 + num3; 
+    p("%d + %d + %d = %d", num1, num2, num3, suma);
 
     return 0;
 }
